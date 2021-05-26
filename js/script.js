a30c53781feed0cc4dfc543e67f7cdfa977fbf08
Ayush <ayushjung63@gmail.com>
2021-05-26 21:09:47 +0545

    first

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+// var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-50px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
+
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//     document.getElementById("navbar").style.backgroundColor="blue";
+//   } else {
+//     document.getElementById("navbar").style.backgroundColor="blue";
+//   }
+// }
+
+// window.onscroll = function() {myFunction()};
+
+// var navbar = document.getElementById("navbar");
+// var sticky = navbar.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.style.backgroundColor="red"
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.style.backgroundColor="blue"
+//     navbar.classList.remove("sticky");
+//   }
+// }
+
+var myNav = document.getElementById('navbar');
+window.onscroll = function () { 
+    "use strict";
+    if (document.body.scrollTop >= 200 ) {
+        myNav.classList.add("nav-colored");
+        myNav.classList.remove("nav-transparent");
+    } 
+    else {
+        myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
+    }
+};
